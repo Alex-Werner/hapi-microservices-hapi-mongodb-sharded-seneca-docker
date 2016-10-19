bfdad694ddadf6f2a985dbece1eb655d1c163f25
Alex Werner <alex@werner.fr>
2016-10-19 02:40:01 +0200

    Call database using seneca

---
+++ services/users/README.md
@@ -0,0 +1,11 @@
+# Users
+
+Run on http protocol on port 11110
+
+## Install 
+
+```npm install```
+
+## Start 
+
+```node index.js```
--- services/users/index.js
+++ services/users/index.js
@@ -1,5 +1,6 @@
 var seneca = require('seneca')();
 const cl = require('khal').cl;
+const ce = require('khal').ce;
 
 var usersOptions = {type: 'http', port: 11110, host: 'localhost'};
 seneca.listen(usersOptions);
@@ -9,8 +10,20 @@ seneca.ready(()=> {
 });
 
 function getUsersList(args, done) {
-    var users = {users: [{username: "AWerner", firstname: "Alex", lastname: "Werner", age: 24}]};
-    done(null,users);
+    const database = require('seneca')();
+    const databaseOptions = {type:'http',port:11100, host:'localhost', pin:{role: 'database', model: 'users', method:'*'}};
+    database.client(databaseOptions);
+    database.ready(function(){
+       cl('connected to database'); 
+    });
+    database.act({
+        role:'database',
+        model:'users',
+        method:'getUsers'
+    },function(err,result){
+        if(err) return ce(err);
+        done(null, result);
+    });
 };
 
 // Temporary, see: https://github.com/senecajs/seneca/issues/566
